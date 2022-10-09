480c2fba0e729dbebdaced90475e996667d2a337
Kaze <Andreas.Keimel@hotmail.de>
2022-10-09 15:39:43 +0200

    push

--- Desktop Fox/MVVM/ViewModels/AddsetVM.cs
+++ Desktop Fox/MVVM/ViewModels/AddsetVM.cs
@@ -12,6 +12,9 @@ using static DesktopFox.DF_Command;
 
 namespace DesktopFox.MVVM.ViewModels
 {
+    /// <summary>
+    /// ViewModel der <see cref="Views.AddSetView"/> Klasse
+    /// </summary>
     public class AddSetVM : ObserverNotifyChange
     {
         public AddSetModel AddSetModel { get; set; }
@@ -19,6 +22,11 @@ namespace DesktopFox.MVVM.ViewModels
         private GalleryManager GM;
         private List<String> fileList;
 
+        /// <summary>
+        /// Konstruktor
+        /// </summary>
+        /// <param name="MainWindowViewModel"></param>
+        /// <param name="galleryManager"></param>
         public AddSetVM(MainWindowVM MainWindowViewModel, GalleryManager galleryManager)
         {
             MWVM = MainWindowViewModel;
@@ -29,24 +37,50 @@ namespace DesktopFox.MVVM.ViewModels
                 AddSetModel = new AddSetModel(NewName());         
         }
 
+        /// <summary>
+        /// Erhält die Benachtrichtigung falls sich das ausgewählte Objekt in der Liste geändert hat. <see cref="MainWindow.lbPictures"/> 
+        /// Aktualisiert den Setnamen in der Anzeige
+        /// </summary>
+        /// <param name="pictureViewVM">Aktuell ausgewähltes Objekt</param>
         public new void ContentChange(PictureVM pictureViewVM)
         {
             AddSetModel.PictureSetName = pictureViewVM.pictureSet.SetName;
         }
 
-        private bool _day;
+        /// <summary>
+        /// Flag ob die Bilder als Tages oder als Nacht Collection hinzugefügt werden sollen.
+        /// </summary>
         public bool Day { get { return _day; } set { _day = value; RaisePropertyChanged(nameof(Day)); } }
+        private bool _day;
+
+        /// <summary>
+        /// Kommando für das öffnen des Ordner Dialogs <see cref="DF_FolderDialog.openFolderDialog"/>
+        /// </summary>
         public ICommand OpenFolderDialog { get { return new DelegateCommand(o => OpenFD()); } }
+
+        /// <summary>
+        /// Kommando für das hinzufügen eines neuen Sets
+        /// </summary>
         public ICommand AddNewSet { get { return new DF_Command.DelegateCommand(o => AddNS()); } }
+
+        /// <summary>
+        /// Kommando für das erstellen eines neuen Setnamens
+        /// </summary>
         public ICommand GenerateSetName { get { return new DF_Command.DelegateCommand(o => GetSetName()); } }
 
+        /// <summary>
+        /// Fügt ein neues Set mit den aktuellen Parametern hinzu.
+        /// </summary>
         private void AddNS()
         {
             if (AddSetModel.FolderPath != null && AddSetModel.PictureSetName != "Error")
                 GM.addSet(AddSetModel.PictureSetName, GalleryManager.makeCollection(fileList, AddSetModel.FolderPath), Day);
-  
         }
 
+        /// <summary>
+        /// Zeigt entweder den Namen des Ausgewählten Sets oder einen neuen Namen in der UI an 
+        /// </summary>
+        /// <param name="newName">Flag ob ein neuer Name erstellt werden soll.</param>
         private void GetSetName(bool newName = true)
         {
             if(!newName && MWVM.SelectedVM != null)
@@ -55,6 +89,10 @@ namespace DesktopFox.MVVM.ViewModels
                 AddSetModel.PictureSetName = NewName();
         }
 
+        /// <summary>
+        /// Ruft den Ordnerdialog auf <see cref="DF_FolderDialog.openFolderDialog"/> 
+        /// und aktualisiert den Ordnerpfad in der UI <see cref="Views.AddSetView.label_SelectedFolder"/>
+        /// </summary>
         private void OpenFD()
         {
             fileList = DF_FolderDialog.openFolderDialog();
@@ -62,6 +100,10 @@ namespace DesktopFox.MVVM.ViewModels
                 AddSetModel.FolderPath = Path.GetDirectoryName(fileList[0]);
         }
 
+        /// <summary>
+        /// Generiert den nächsten Set Namen der verfügbar ist und gibt diesen zurück
+        /// </summary>
+        /// <returns></returns>
         private String NewName()
         {
             String newName;
@@ -85,11 +127,5 @@ namespace DesktopFox.MVVM.ViewModels
             Debug.WriteLine("Maximaler Anzahl von NewSets erreicht. New Name = " + newName);
             return newName;
         }
-
-        private void NewPS()
-        {
-
-        }
-
     }
 }

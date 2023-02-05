68c9900bbe9d019eb176ef31a41e5e6bb03202d1
Kaze <Andreas.Keimel@hotmail.de>
2023-02-05 18:22:14 +0100

    Kommentar hinzugefügt

--- Desktop Fox/Helper/CheckDaySwitch.cs
+++ Desktop Fox/Helper/CheckDaySwitch.cs
@@ -1,13 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace DesktopFox
 {
+    /// <summary>
+    /// Helferklasse für den Tageswechsel Check
+    /// </summary>
     public static class CheckDaySwitch
     {
+        /// <summary>
+        /// Ermitteln ob ein Tageswechsel durchgeführt werden muss und ermittlung des nächsten Tageswechsels
+        /// </summary>
+        /// <param name="now">Aktuelle Zeit von der Ausgegangen werden soll. Sollte Default <see cref="DateTime.Now"/> sein</param>
+        /// <param name="nextSwitch">Tageswechselzeit die geprüft werden soll</param>
+        /// <returns></returns>
         public static DateTime Check(DateTime now, DateTime nextSwitch)
         {
             if (now < nextSwitch && Math.Abs((nextSwitch - now).Days) < 1) return nextSwitch;

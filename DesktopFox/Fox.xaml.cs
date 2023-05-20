282ed6851f1fa0185727ff8f69d32f1ccf3ca495
Kaze <Andreas.Keimel@hotmail.de>
2023-05-20 21:02:12 +0200

    Disable Logging

--- DesktopFox/Fox.xaml.cs
+++ DesktopFox/Fox.xaml.cs
@@ -231,7 +231,7 @@ namespace DesktopFox
                 DF_Json.deleteFile(SaveFileType.Wallpaper);
             }
 
-            if (DF_Json.saveFile(gallery, logging: true) & DF_Json.saveFile(settings, logging: true))
+            if (DF_Json.saveFile(gallery) & DF_Json.saveFile(settings))
                 Debug.WriteLine("App Close. Speichern der Daten war erfolgreich");
             else
                 Debug.WriteLine("App Close. FEHLER. Daten konnten nicht gespeichert werden.");

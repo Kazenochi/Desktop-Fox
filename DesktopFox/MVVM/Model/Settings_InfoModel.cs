fee67d88338d7db6d5e331facdbed27f7b854e47
Kaze <Andreas.Keimel@hotmail.de>
2023-06-10 12:50:41 +0200

    Official version & license change

--- DesktopFox/MVVM/Model/Settings_InfoModel.cs
+++ DesktopFox/MVVM/Model/Settings_InfoModel.cs
@@ -12,7 +12,7 @@ namespace DesktopFox.MVVM.Model
         private string _appName = System.AppDomain.CurrentDomain.FriendlyName;
 
         public string AppVersion { get { return _appVersion; } set { _appVersion = value; RaisePropertyChanged(nameof(AppVersion)); } }
-        public string _appVersion { get; set;} = "Beta 0.1";
+        public string _appVersion { get; set;} = "Beta 1.0";
 
         public string FrameworkVersion { get { return _frameworkVersion; } set { _frameworkVersion = value; RaisePropertyChanged(nameof(FrameworkVersion)); } }
         public string _frameworkVersion { get; set; } = Environment.Version.ToString();
@@ -21,6 +21,6 @@ namespace DesktopFox.MVVM.Model
         public string _developer { get; set; } = "Andreas Keimel";
 
         public string Licence { get { return _licence; } set { _licence = value; RaisePropertyChanged(nameof(Licence)); } }
-        public string _licence { get; set; } = "MIT"; 
+        public string _licence { get; set; } = "GPL"; 
     }
 }
